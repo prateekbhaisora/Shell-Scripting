d5b0a299595ef88296113e14d94173d6b3b073d9
Prateek Bhaisora <prateek@Prateeks-MacBook-Air.local>
2024-01-23 19:26:10 +0530

    For loops

---
+++ 14_forloop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+for i in 1 2 3 4 5
+do
+	echo "Number is: $i"
+done
+
+for name in Prateek Singh Bhaisora
+do
+	echo "Name is: $name"
+done
+
+for p in {2..7}
+do
+	echo "Val is: $p "
+done
